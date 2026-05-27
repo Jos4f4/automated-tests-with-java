a8a9a56fad7830f168000fd8d964908e2025db45
Josaf4 <josafa.soares2909@gmail.com>
2026-05-27 00:01:45 -0400

    Introduction in the TDD - Test Drive Development

---
+++ automated-tests-with-java/src/test/java/br/com/erudio/PersonServiceTest.java
@@ -0,0 +1,18 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+	
+	@Test
+	@DisplayName("When Create a Person with Sucess Should Return a Person Object")
+	void testCreatePerson_WhenSucess_ShouldReturnPersonObject() {
+		
+		// Given / Arrange
+		
+		// When / Act
+		
+		// Then / Assert
+	}
+}
