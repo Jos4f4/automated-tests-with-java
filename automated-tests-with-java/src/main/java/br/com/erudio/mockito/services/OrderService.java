5c692ab8c2de80df4937189af8afffaac5a3771a
Josaf4 <josafa.soares2909@gmail.com>
2026-06-11 01:28:41 -0400

    mocking static method and LocalDateTime

---
+++ automated-tests-with-java/src/main/java/br/com/erudio/mockito/services/OrderService.java
@@ -0,0 +1,23 @@
+package br.com.erudio.mockito.services;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class OrderService {
+	
+	public Order createOrder(String productName, Long amount, String orderId) {
+		
+		Order order = new Order();
+		
+		order.setId(orderId == null ? UUID.randomUUID().toString() : orderId);
+		
+		order.setCreationDate(LocalDateTime.now());
+		
+		order.setAmount(amount);
+		
+		order.setProdutcName(productName);
+		
+		return order;
+	}
+
+}
